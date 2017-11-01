a136d0b272cac0bd652939767742e8acd97b58e5
AkulichAleksandr <Akulich.Aleksandr@gmail.com>
2017-11-01 11:51:52 +0300

    The sequence of the requests was corrected

--- JS_Task07/XMLHttpRequestWithPromise/XMLHttpRequestWithPromise.js
+++ JS_Task07/XMLHttpRequestWithPromise/XMLHttpRequestWithPromise.js
@@ -1,16 +1,16 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var arrayOfURL = ['http://www.nbrb.by/API/ExRates/Rates/978?ParamMode=1',
-    'http://www.nbrb.by/API/ExRates/Rates/RUB?ParamMode=2',
-    'http://www.nbrb.by/API/ExRates/Rates/145',
-    'http://www.nbrb.by/API/ExRates/Rates?onDate=2011-11-11&Periodicity=1',
-    'http://www.nbrb.by/API/ExRates/Rates?Periodicity=0'];
+ var arrayOfURL = ['http://www.nbrb.by/API/ExRates/Rates/978?ParamMode=1',
+                   'http://www.nbrb.by/API/ExRates/Rates/RUB?ParamMode=2',
+                   'http://www.nbrb.by/API/ExRates/Rates/145',
+                   'http://www.nbrb.by/API/ExRates/Rates?onDate=2011-11-11&Periodicity=1',
+                   'http://www.nbrb.by/API/ExRates/Rates?Periodicity=0'];
 
 function httpGet(url) {
     return new Promise(function (resolve, reject) {
 
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', url, false);
+        xhr.open('GET', url, true);
 
         xhr.onload = function () {
             if (this.status === 200) {
@@ -30,12 +30,36 @@ function httpGet(url) {
     });
 }
 
-arrayOfURL.forEach(function (link) {
-    console.log(link);
-    httpGet(link)
-        .then((function (response) {
+console.log(arrayOfURL[0]);
+httpGet(arrayOfURL[0]) 
+    .then( 
+        response => { 
             console.log(response + '\nRequest is completed successful\n==============================================================================');
-        }), function (err) {
-            console.log(err.message + '\nBad request\n==============================================================================');
-        })
-});
+            console.log(arrayOfURL[1]); 
+            return httpGet(arrayOfURL[1]) 
+        }, 
+        error => console.log(error.message + '\nBad request\n=============================================================================='))
+        .then( 
+            response => { 
+                console.log(response + '\nRequest is completed successful\n=============================================================================='); 
+                console.log(arrayOfURL[2]);
+                return httpGet(arrayOfURL[2]) 
+            }, 
+            error => console.log(error.message + '\nBad request\n=============================================================================='))
+            .then( 
+                response => { 
+                    console.log(response + '\nRequest is completed successful\n=============================================================================='); 
+                    console.log(arrayOfURL[3]);
+                    return httpGet(arrayOfURL[3]) 
+                }, 
+                error => console.log(error.message + '\nBad request\n=============================================================================='))
+                .then( 
+                    response => { 
+                        console.log(response + '\nRequest is completed successful\n=============================================================================='); 
+                        console.log(arrayOfURL[4]);
+                        return httpGet(arrayOfURL[4]) 
+                }, 
+                    error => console.log(error.message + '\nBad request\n=============================================================================='))
+                    .then( 
+                        response => console.log(response + '\nFINISH\n=============================================================================='), 
+                        error => console.log(error.message + '\nFINISH\n=============================================================================='))
--- JS_Task07/XMLHttpRequestWithoutPromise/XMLHttpRequestWithoutPromise.js
+++ JS_Task07/XMLHttpRequestWithoutPromise/XMLHttpRequestWithoutPromise.js
@@ -1,26 +1,120 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
 var arrayOfURL = ['http://www.nbrb.by/API/ExRates/Rates/978?ParamMode=1',
-               'http://www.nbrb.by/API/ExRates/Rates/RUB?ParamMode=2',
-               'http://www.nbrb.by/API/ExRates/Rates/145',
-               'http://www.nbrb.by/API/ExRates/Rates?onDate=2011-11-11&Periodicity=1',
-               'http://www.nbrb.by/API/ExRates/Rates?Periodicity=0'];
+    'http://www.nbrb.by/API/ExRates/Rates/RUB?ParamMode=2',
+    'http://www.nbrb.by/API/ExRates/Rates/145',
+    'http://www.nbrb.by/API/ExRates/Rates?onDate=2011-11-11&Periodicity=1',
+    'http://www.nbrb.by/API/ExRates/Rates?Periodicity=0'];
 
-xhr.onreadystatechange = function () {
-    if (xhr.readyState !== 4) return;
+var xhr1 = new XMLHttpRequest();
+function request1(callback) {
+    xhr1.open('GET', arrayOfURL[0], true);
+    xhr1.onreadystatechange = function () {
+        if (xhr1.readyState != 4) return;
+        if (xhr1.status != 200) {
+            console.log('==========\nRequest 1 to the server');
+            console.log(xhr1.status + ': ' + xhr1.statusText);
+        } else {
+            console.log('==========\nRequest 1 to the server');
+            console.log(xhr1.responseText);
+        }
+        if (callback && typeof callback === 'function') {
+            callback();
+        }
+    };
+    xhr1.send();
+};
 
-    if (xhr.status !== 200) {
-        console.log(xhr.status + ': ' + xhr.statusText + '\nBad request\n==============================================================================');
-    } else {
-        console.log(xhr.responseText);
-        console.log('Request is completed successful\n==============================================================================');
-        
+var xhr2 = new XMLHttpRequest();
+function request2(callback) {
+    xhr2.open('GET', arrayOfURL[1], true);
+    xhr2.onreadystatechange = function () {
+        if (xhr2.readyState != 4) return;
+        if (xhr2.status != 200) {
+            console.log('==========\nRequest 2 to the server');
+            console.log(xhr2.status + ': ' + xhr2.statusText);
+        } else {
+            console.log('==========\nRequest 2 to the server');
+            console.log(xhr2.responseText);
+        }
+        if (callback && typeof callback === 'function') {
+            callback();
+        }
+    };
+    xhr2.send();
+};
+
+var xhr3 = new XMLHttpRequest();
+function request3(callback) {
+    xhr3.open('GET', arrayOfURL[2], true);
+    xhr3.onreadystatechange = function () {
+        if (xhr3.readyState != 4) return;
+        if (xhr3.status != 200) {
+            console.log('==========\nRequest 3 to the server');
+            console.log(xhr3.status + ': ' + xhr3.statusText);
+        } else {
+            console.log('==========\nRequest 3 to the server');
+            console.log(xhr3.responseText);
+        }
+        if (callback && typeof callback === 'function') {
+            callback();
+        }
+    };
+    xhr3.send();
+};
+
+var xhr4 = new XMLHttpRequest();
+function request4(callback) {
+    xhr4.open('GET', arrayOfURL[3], true);
+    xhr4.onreadystatechange = function () {
+        if (xhr4.readyState != 4) return;
+        if (xhr4.status != 200) {
+            console.log('==========\nRequest 4 to the server');
+            console.log(xhr4.status + ': ' + xhr4.statusText);
+        } else {
+            console.log('==========\nRequest 4 to the server');
+            console.log(xhr4.responseText);
+        }
+        if (callback && typeof callback === 'function') {
+            callback();
+        }
+    };
+    xhr4.send();
+};
+
+var xhr5 = new XMLHttpRequest();
+function request5(callback) {
+    xhr5.open('GET', arrayOfURL[4], true);
+    xhr5.onreadystatechange = function () {
+        if (xhr5.readyState != 4) return;
+        if (xhr5.status != 200) {
+            console.log('==========\nRequest 5 to the server');
+            console.log(xhr5.status + ': ' + xhr5.statusText);
+        } else {
+            console.log('==========\nRequest 5 to the server');
+            console.log(xhr5.responseText);
+        }
+        if (callback && typeof callback === 'function') {
+            callback();
+        }
+    };
+    xhr5.send();
+};
+
+var num = 0;
+var arrs = [];
+
+function callAsync(arr) {
+    if (arr) {
+        arrs = arr;
     }
-}
+    if (arrs[num]) {
+        arrs[num](callAsync);
+        num++;
+    } else {
+        arrs = [];
+        num = 0;
+    }
+};
 
-arrayOfURL.forEach(function (link) {
-    console.log(link);
-    xhr.open('GET', link, false);
-    xhr.send();
-    xhr.onreadystatechange();
-})
+callAsync([request1, request2, request3, request4, request5]);
